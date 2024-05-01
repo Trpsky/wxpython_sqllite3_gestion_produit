24435ac6f2633612134225aaef3960613887c227
ELMEHDI ACHAHED <117928790+Trpsky@users.noreply.github.com>
2024-05-01 09:17:23 +0100

    merging the files into one single file script.py start implemeting the admin add  gui and database query

--- script.py
+++ script.py
@@ -1,23 +1,44 @@
 import wx
+import sqlite3
 
-class MyFrame(wx.Frame):
+class AddUserForm(wx.Frame):
     def __init__(self, parent, title):
-        super(MyFrame, self).__init__(parent, title=title, size=(400, 300))
+        super(AddUserForm, self).__init__(parent, title=title, size=(300, 200))
 
         panel = wx.Panel(self)
-        btn = wx.Button(panel, label='Click Me')
-        btn.Bind(wx.EVT_BUTTON, self.on_button_click)
-
         sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(btn, 0, wx.ALL | wx.CENTER, 20)
-        panel.SetSizer(sizer)
 
+        self.name_label = wx.StaticText(panel, label='Nom:')
+        self.first_name = wx.TextCtrl(panel)
+        sizer.Add(self.name_label, 0, wx.ALL | wx.EXPAND, 5)
+        sizer.Add(self.first_name, 0, wx.ALL | wx.EXPAND, 5)
+
+        self.age_label = wx.StaticText(panel, label='Prenom:')
+        self.last_name = wx.TextCtrl(panel)
+        sizer.Add(self.age_label, 0, wx.ALL | wx.EXPAND, 5)
+        sizer.Add(self.last_name, 0, wx.ALL | wx.EXPAND, 5)
+
+        self.submit_btn = wx.Button(panel, label='Ajouter Admin')
+        self.submit_btn.Bind(wx.EVT_BUTTON, self.on_submit)
+        sizer.Add(self.submit_btn, 0, wx.ALL | wx.CENTER, 10)
+
+        panel.SetSizer(sizer)
         self.Show()
 
-    def on_button_click(self, event):
-        wx.MessageBox('Button clicked!')
+    def on_submit(self, event):
+        first_name = self.first_name.GetValue()
+        last_name = self.last_name.GetValue()
+
+        db = sqlite3.connect('gestion_produit.db')
+        cursor = db.cursor()
+        cursor.execute('CREATE TABLE IF NOT EXISTS admins (id INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT)')
+        cursor.execute('INSERT INTO admins (first_name, last_name) VALUES (?, ?)', (first_name, last_name))
+        db.commit()
+        db.close()
+
+        wx.MessageBox('Admin est ete ajouter a la base de donne!')
 
 if __name__ == '__main__':
     app = wx.App()
-    frame = MyFrame(None, 'My App')
+    AddUserForm(None, title='Ajouter une nouvelle admin')
     app.MainLoop()
